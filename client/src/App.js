cd2aa71a01a94886776fbb87a4841ff8874c64e8
Steven Green <ste2144105@gmail.com>
2023-01-14 03:03:28 -0700

    setup app.js and index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Scheduler from "./components/Scheduler";
+import Appointments from "./components/Appointments";
+import Login from "./components/Login";
 
 function App() {
-  return <div className="App"></div>;
+	const [currentTab, setCurrentTab] = useState("login");
+
+	const renderTab = () => {
+		switch (currentTab) {
+			case "home":
+				return <Home />;
+			case "scheduler":
+				return <Scheduler />;
+			case "appointments":
+				return <Appointments />;
+			case "login":
+				return <Login />;
+			default:
+				return null;
+		}
+	};
+
+	return (
+		<div>
+			<div>
+				<Header setCurrentTab={setCurrentTab}></Header>
+			</div>
+			<div>
+				<main>{renderTab()}</main>
+			</div>
+		</div>
+	);
 }
 
 export default App;
