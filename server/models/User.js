00f53da740df3cc79c6a709490a309e69ef7ef8d
landycodes <landryandrewsk8@gmail.com>
2023-01-17 15:19:26 -0700

    resolvers without context tested and functional

--- server/models/User.js
+++ server/models/User.js
@@ -32,12 +32,12 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
   if (this.isNew || this.isModified("password")) {
     const saltRounds = 10;
-    this.password = await bcrypt.hash;
-    this.password, saltRounds;
+    this.password = await bcrypt.hash(this.password, saltRounds);
   }
   next();
 });
 
+//validate password
 userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
