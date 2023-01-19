3312aa81120b59e8c3e2ba3362c84da0d99c19bc
Steven Green <ste2144105@gmail.com>
2023-01-18 19:28:11 -0700

    updated nav and login token

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -27,7 +27,7 @@ const Login = (props) => {
           variables: { ...formState },
         });
   
-        Auth.login(data.login);
+        Auth.login(data.login.token);
       } catch (e) {
         console.error(e);
       }
