aa003e282921235b1b79bfa132477dfa6767bf96
taylorzak <taylorzak@hotmail.com>
2023-01-19 20:24:27 -0700

    finished quotes

--- client/src/utils/API.js
+++
@@ -1,7 +0,0 @@
-
-import axios from 'axios';
-
-const search = (query) =>
-  axios.get(`https://api.api-ninjas.com/v1/quotes?category=${query}',{headers:{"X-Api-Key":"yCMvFpxTzoD1YpyLGRHvfg==uQKwGLSO5PxwmmxN`);
-
-export default search;
