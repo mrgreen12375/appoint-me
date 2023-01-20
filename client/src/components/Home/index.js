cd014c0c11a3f15237617af3277cbf564b94cc1a
FixieGemini <109640240+FixieGemini@users.noreply.github.com>
2023-01-19 20:32:49 -0700

    cleaned up homepage welcome

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -5,7 +5,12 @@ function Home() {
 
 	return (
 	<main>
-		<h2 className="title"> {Auth.loggedIn() ? (<h2 className="title">Welcome, Steven</h2>) : (<h2 className="title">Please sign in</h2>)} </h2>
+		<h2 className="title"> 
+			{Auth.loggedIn() 
+				? (<h2 className="title">Welcome</h2>)
+				: (<h2 className="title"> </h2>)
+			} 
+		</h2>
 		<div className="inspiration">
 			<h2>Inspirational Quote</h2>
 			<p className="quote">“When one door of happiness closes, another opens; but often we look so long at the closed door that we do not see the one which has been opened for us.”</p>
