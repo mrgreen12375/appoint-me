d8bda1acded9d7f32355d963c5262384085bfe2f
Steven Green <stevengreen@Stevens-MacBook-Pro.local>
2025-02-28 16:51:41 -0700

    updated quote api fetch

--- client/src/components/Quote/index.js
+++ client/src/components/Quote/index.js
@@ -1,33 +1,45 @@
 import React, { useEffect, useState } from "react";
 
-const query = "inspirational";
-
 function Quote() {
-  const [quote, setQuote] = useState();
-  const [author, setAuthor] = useState();
+  const [quote, setQuote] = useState("Loading...");
+  const [author, setAuthor] = useState("Unknown");
 
   useEffect(() => {
     getQuoteApiData();
   }, []);
 
   const getQuoteApiData = async () => {
-    fetch(`https://api.api-ninjas.com/v1/quotes?category=${query}`, {
-      headers: {
-        "X-Api-Key": "yCMvFpxTzoD1YpyLGRHvfg==uQKwGLSO5PxwmmxN",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // console.log(data[0]);
+    try {
+      const response = await fetch(`https://api.api-ninjas.com/v1/quotes`, {
+        headers: {
+          "X-Api-Key": "1D64zSLzO3OusnJsGZQERg==t3NWmkZDQBLGEADZ",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      if (data.length > 0) {
         setQuote(data[0].quote);
         setAuthor(data[0].author);
-      });
+      } else {
+        setQuote("No quote found.");
+        setAuthor("Unknown");
+      }
+    } catch (error) {
+      console.error("Error fetching quote:", error);
+      setQuote("Failed to fetch quote.");
+      setAuthor("Error");
+    }
   };
 
   return (
     <div className="inspiration">
       <p className="quote">{quote}</p>
-      <p className="author">-{author}</p>
+      <p className="author">- {author}</p>
     </div>
   );
 }
